7d422d81f2472766d28524a35d01f6c6c520e10c
Baiyu Li <baiyuli97@gmail.com>
2019-05-03 14:01:07 -0700

    organized data files

--- Autocomplete.java
+++ Autocomplete.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Autocomplete {
 	public static void main(String[] args) throws IOException { 
-		LMModel languageModel = new DiscountLMModel(args[0], Double.parseDouble(args[2]));
-		System.out.println("Perplexity: " + languageModel.getPerplexity(args[1]));
-		languageModel.returnPredictions(args[1]);
+		LMModel languageModel = new DiscountLMModel("data/" + args[0], Double.parseDouble(args[2]));
+		System.out.println("Perplexity: " + languageModel.getPerplexity("data/" + args[1]));
+		languageModel.returnPredictions("data/" + args[1]);
 	}
 }
